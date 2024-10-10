a821b759af8894fc08bdc3f21e66597aad91f0e0
Dominik Dadański <ddadanski@edu.cdv.pl>
2024-10-10 12:45:44 +0200

    Core installers

---
+++ Assets/Core/Code/Backend/Installers/CoreInstaller.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Zenject;
+
+public class CoreInstaller : Installer<CoreInstaller>
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<IMoodService>().To<MoodService>().AsSingle();
+    }
+}
